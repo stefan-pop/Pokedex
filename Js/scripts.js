96276a12d48d9169f6ec308f8ba6bc3bb52f4f98
Stefan Pop <stefan9161@gmail.com>
2021-04-05 16:51:07 +0200

    created a variable with 3 objects placed inside an array

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,1 +1,25 @@
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'posion']
+    },
 
+    {
+        name: 'Weedle',
+        height: 3,
+        types: ['bug', 'poison']
+    },
+    
+    {
+        name: 'Golbat',
+        height: 16,
+        types: ['poison', 'flying']
+    },
+
+    {
+        name: 'Doduo',
+        height: 14,
+        types: ['flying', 'normal'] 
+    }
+]
