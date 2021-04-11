d4ed3d68a4f5bde0affad6c5a95239f20ad31f0b
Stefan Pop <stefan9161@gmail.com>
2021-04-11 14:27:43 +0200

    wrapped the pokemonList in an IIFE and added 2 function
    The 'add' function addes to the pokemonList.
    The 'getAll' function returns the pokemonList.

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,25 +1,24 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 7,
-        types: ['grass','posion']
-    },
-    {
-        name: 'Weedle',
-        height: 3,
-        types: ['bug','poison']
-    },
-    {
-        name: 'Golbat',
-        height: 16,
-        types: ['poison','flying']
-    },
-    {
-        name: 'Doduo',
-        height: 14,
-        types: ['flying','normal'] 
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {name: 'Bulbasaur', height: 7, types: ['grass','posion']},
+        {name: 'Weedle', height: 3, types: ['bug','poison']},
+        {name: 'Golbat', height: 16, types: ['poison','flying']},
+        {name: 'Doduo', height: 14, types: ['flying','normal'] }
+    ]
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-]
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    }
+})();
 
 // 'forEach' loop that iterates through the list and returns the 'name' and the 'height' of each object in the list. If the height of an object is greater
 // than 16, it will also add the 'highlightNote' variable.
