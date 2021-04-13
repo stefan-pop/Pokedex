8afd3b3d2f4e0c6a2154be6b6ee40adc4fa71f40
Stefan Pop <stefan9161@gmail.com>
2021-04-13 16:53:45 +0200

    deleted the old unnecessary code

--- Js/scripts.js
+++ Js/scripts.js
@@ -46,16 +46,7 @@ let pokemonRepository = (function() {
 // 'forEach' loop that iterates through the list and returns the 'name' and the 'height' of each object in the list. If the height of an object is greater
 // than 16, it will also add the 'highlightNote' variable.
 
-let highlightNote = " -Wow that's big!";
 
-document.write('<ul class="pokemon-list">');  // created a <ul> that will contain the <li> created by if else statements
 
 pokemonRepository.getAll().forEach(function(item) {
-    if(item.height >= 16){
-        document.write('<li>'+'<h1>'+ item.name +'</h1>' + '<p>' + ' Height: '+ item.height +  `${highlightNote}`+ '</p>' + '</li>');
-    }else{
-        document.write('<li>'+'<h1>' + item.name +'</h1>' + '<p>' +' Height: '+ item.height + '</p>' +'</li>');
-    }
 });
-
-document.write('</ul');  // closed the </ul> here so that it will wrap all the <li> elements
