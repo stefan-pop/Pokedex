a9f1580ccd576f0cd12bf47228e4f673f9422900
Stefan Pop <stefan9161@gmail.com>
2021-04-15 17:15:00 +0200

    added a function that fetches the URL of each pokemon object to get its details and assign them to it.

--- Js/scripts.js
+++ Js/scripts.js
@@ -48,6 +48,20 @@ let pokemonRepository = (function() {
             console.log(error);
         })
     }
+    
+    // function that loads the details by fetching the url of each pokemon
+    function loadDetails(y) {
+        let url = y.detailsUrl;
+        return fetch(url).then(function(response) {
+            return response.json();
+        }).then(function(details) {
+            y.imageUrl = details.sprites.front_default;
+            y.height = details.height;
+            y.types = details.types;
+        }).catch(function(error) {
+            console.log(error);
+        })
+    }
 
     return {
         add: add,
